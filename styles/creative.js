565bf1b1391ad3ded90bda408f5d6c55f1ddf320
ritztoston <ritztoston@gmail.com>
2018-07-16 20:44:10 +0800

    Initial commit

---
+++ styles/creative.js
@@ -0,0 +1,28 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      let target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+// When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400 ) {
+      document.getElementById("top-button").style.display = "block";
+    } else {
+      document.getElementById("top-button").style.display = "none";
+    }
+  }
+})(jQuery); // End of use strict
